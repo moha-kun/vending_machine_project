e5345d2ca4741420f2391eb4687f03daefbaf766
moha-kun <naitmoussamohamed@gmail.com>
2024-02-22 23:18:50 +0100

    add readme file

---
+++ readme.md
@@ -0,0 +1,8 @@
+### Java assignment
+
+You need to design a Vending Machine which
+- [x] Accepts coins of 1, 2, 5 and 10 dirham coins (not the 0.5 dirham coin)
+- [x] Allow user to select products water(5), coca(7), twix(10), bueno(12)
+- [x] Allow user to take refund by canceling the request.
+- [x] Return the selected product and remaining change if any
+- [] Allow reset operation for vending machine supplier.
--- src/test/java/org/exemple/VendingMachineTest.java
+++ src/test/java/org/exemple/VendingMachineTest.java
@@ -10,15 +10,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.Map;
 import java.util.stream.Stream;
 
-/*
- You need to design a Vending Machine which
-     [x]Accepts coins of 1, 2, 5 and 10 dirham coins (not the 0.5 dirham coin)
-     [x]Allow user to select products water(5), coca(7), twix(10), bueno(12)
-     [x]Allow user to take refund by canceling the request.
-     [x]Return the selected product and remaining change if any
-     []Allow reset operation for vending machine supplier
-*/
-
 public class VendingMachineTest {
 
     private VendingMachine vendingMachine;
